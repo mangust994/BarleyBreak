647f074e1ffb40d719fa8e406e9c0cf9c6cea2b7
dehtiar <mister.degtyar@mail.ru>
2021-06-09 16:21:10 +0300

    Up token life-time

--- BarleyBreakApi/AuthOptions.cs
+++ BarleyBreakApi/AuthOptions.cs
@@ -8,7 +8,7 @@ namespace BarleyBreakApi
         public const string ISSUER = "dotNetServer";
         public const string AUDIENCE = "AngularClient";
         const string KEY = "mysupersecret_secretkey!123";
-        public const int LIFETIME = 10;
+        public const int LIFETIME = 240;
         public static SymmetricSecurityKey GetSymmetricSecurityKey()
         {
             return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
